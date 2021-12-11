eb365e08694f9be77e7099fd38555f4a7cb37807
ALEX <94417808+AlSch092@users.noreply.github.com>
2021-12-10 21:10:04 -0500

    added get unique name for items

--- Components/Mods.cpp
+++ Components/Mods.cpp
@@ -56,3 +56,53 @@ int Mods::GetRequiredLevel(Entity* e)
 
 	return requiredLevel;
 }
+
+wchar_t* Mods::GetUniqueName(Entity* e)
+{
+	UINT64 CompAddr = e->GetComponentAddress("Mods");
+	wchar_t NamePtr[100] = { 0 };
+
+	if (CompAddr != NULL)
+	{
+		CompAddr = CompAddr + Mods::uniqueNameOffset;
+
+		if (CompAddr != NULL)
+		{
+			int Count = 0;
+			UINT64 PtrListArray = DereferenceSafe<UINT64>(CompAddr);
+			UINT64 PtrEndArray = DereferenceSafe<UINT64>(CompAddr + 8);
+
+			UINT64 FirstNameRecord = DereferenceSafe<UINT64>(PtrListArray);
+			UINT64 Current = FirstNameRecord;
+
+			printf("PtrListArray: %llX\n", PtrListArray);
+			printf("PtrEndArray: %llX\n", PtrEndArray);
+			printf("Current: %llX\n", Current);
+
+			while (Current < PtrEndArray)
+			{
+				int NameRecordCounter = DereferenceSafe<int>(Current);
+				UINT64 NameAddr = DereferenceSafe<UINT64>(Current + 4);
+				
+				printf("Count: %d, NameAddr %llX\n", NameRecordCounter, NameAddr);
+
+				if (NameAddr != NULL)
+				{
+					if (Count == 1)
+					{
+						wcscpy(NamePtr, (const wchar_t*) NameAddr);
+					}
+					else
+					{
+						wcscat(NamePtr, (const wchar_t*)NameAddr);
+					}
+				}
+
+				Current += 0x10;
+			}
+		}
+
+	}
+
+	return NamePtr;
+}
