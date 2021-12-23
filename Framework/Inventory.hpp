acfce0c8fc113bb59befc0d8535cc87e4609df88
ALEX <94417808+AlSch092@users.noreply.github.com>
2021-12-22 23:54:27 -0500

    working towards polymorphism for inventories

--- Framework/Inventory.hpp
+++ Framework/Inventory.hpp
@@ -2,7 +2,7 @@
 #include "Element.hpp"
 #include "AutoPlayer.hpp"
 
-namespace Inventory
+namespace Inventory //inherited class from elemnt
 {
 	static const UINT64 PickupIndex = 0x420;
 	static const UINT64 EntityOffset = 0x428;
@@ -11,7 +11,13 @@ namespace Inventory
 	static const UINT64 CellWidthOffset = 0x438;
 	static const UINT64 CellHeightOffset = CellWidthOffset + 4;
 	static const UINT64 IsHoveredOffset = 0x496;
-	static const UINT64 AffinityFlagsOffset = 0x4D0;
+	static const UINT64 AffinityFlagsOffset = 0x4D0; //only for inventories.. not trade windows?
+
+	static const UINT64 TradeWindowOffset_OurItems = 0x458;//1st offset
+	static const UINT64 TradeWindowOffset_TheirItems = 0x460;
+
+	static const UINT64 TradeWindowOffset_OurItems2 = 0x6B8; //1st offset ... does this mean the new windows popping up become the first tabs of stash??
+	static const UINT64 TradeWindowOffset_TheirItems2 = 0x6C0;
 
 	//this ptr will have most if not all important windows, gg
 	static const UINT64 CurrencyTabOffset = 0x4D0; //redo this for hopefully a smaller solution
@@ -20,12 +26,12 @@ namespace Inventory
 	static const UINT64 StashTab3Offset = StashTab2Offset + 8;
 	static const UINT64 StashTab4Offset = StashTab3Offset + 8;
 
-	static const UINT64 TradeWindowOffset_OurItems = 0x6C0;
-	static const UINT64 TradeWindowOffset_TheirItems = TradeWindowOffset_OurItems - 8;
+	static const UINT64 NPCTradeWindowOffset_OurItems = 0x6C0;
+	static const UINT64 NPCTradeWindowOffset_TheirItems = TradeWindowOffset_OurItems - 8;
 
 	enum TradeWindowSide
 	{
-		Other = 0,
+		Remote = 0,
 		Local
 	};
 
@@ -66,14 +72,27 @@ namespace Inventory
 	struct Item
 	{
 		Entity* ItemEnt;
+		
 		int CellX;
 		int CellY;
+		
 		int Height;
 		int Width;
+		
 		bool isHovered;
+		
 		int StashPage;
+		
 		UINT64 MemoryAddress;
 		UINT32 ElementId;
+		
+		int stackSize;
+		
+		wstring UniqueName;
+		wstring BaseName;
+		wstring FullName; //Unique name + Base Name
+		wstring PriceTag;
+
 	};
 	
 }
@@ -90,7 +109,7 @@ public:
 	std::list<Inventory::Item*> Items;
 	UINT64 ElementAddress;
 
-
+	virtual bool DoesItemFitInSlots(Inventory::Item* itm, int CellX, int CellY) { return false; };
 };
 
 class NormalInventory : public InventoryBase
@@ -98,41 +117,71 @@ class NormalInventory : public InventoryBase
 public:
 
 	NormalInventory() {};
-
-	void SetTabPermissions(byte p);
-	void SetTabFlags(byte f);
-	void SetTabAffinityFlags(UINT16 af);
-
-	UINT16 GetAffinityFlags(UINT64 ElementAddr) { UINT16 af = DereferenceSafe<UINT16>(ElementAddr + Inventory::AffinityFlagsOffset); return af; }
+	
 	std::list<Inventory::Item*> GetInventoryItems();
 	Inventory::Item* GetInventoryItem(UINT64 StructAddr);
 
-private:
-	Inventory::InventoryTabPermissions Permissions;
-	Inventory::InventoryTabFlags Flags;
-	Inventory::InventoryTabAffinityFlags AffinityFlags;
+	virtual bool DoesItemFitInSlots(Inventory::Item* itm, int CellX, int CellY) { return false; }
 
-};
+	virtual ~NormalInventory() {};
 
-class CurrencyInventory : public InventoryBase
-{
-public:
-	std::list<Inventory::Item*> GetInventoryItems();
-private:
-	Inventory::InventoryTabPermissions Permissions;
-	Inventory::InventoryTabFlags Flags;
-	Inventory::InventoryTabAffinityFlags AffinityFlags;
+	friend NormalInventory operator+= (NormalInventory lhs, Inventory::Item* rhs) //confusing
+ 	{
+		lhs.Items.push_back(rhs);
+	}
+
 };
 
 class StashInventory : public NormalInventory
 {
 public:
+	void SetTabPermissions(byte p);
+	void SetTabFlags(byte f);
+	void SetTabAffinityFlags(UINT16 af);
+
+	UINT16 GetAffinityFlags(UINT64 ElementAddr) { UINT16 af = DereferenceSafe<UINT16>(ElementAddr + Inventory::AffinityFlagsOffset); return af; }
+
 	std::list<Inventory::Item*> GetInventoryItemsAtTab(int index);
 	void ForceLoadTab(int index);
+
+	int StashTabPage;
+	string StashTabNickname;
+
+	Inventory::InventoryTabPermissions Permissions;
+	Inventory::InventoryTabFlags Flags;
+	Inventory::InventoryTabAffinityFlags AffinityFlags;
+
+	virtual bool DoesItemFitInSlots(Inventory::Item* itm, int CellX, int CellY) { return false; }
+
+	virtual ~StashInventory() {};
+};
+
+class CurrencyInventory : public StashInventory
+{
+public:
+	std::list<Inventory::Item*> GetInventoryItems();
+
+	int ChaosOrbs, ExaltedOrbs, VaalOrbs, FusingOrbs, ChromaticOrbs = { 0 };
+
+	virtual bool DoesItemFitInSlots(Inventory::Item* itm, int CellX, int CellY) { return false; }
+
+	friend CurrencyInventory operator+= (CurrencyInventory lhs, Inventory::Item* rhs) //confusing
+	{
+		lhs.Items.push_back(rhs);
+	}
+
+	virtual ~CurrencyInventory() {};
 };
 
 class TradeInventory : public InventoryBase
 {
 public:
-	std::list<Inventory::Item*> GetTradeWindowItems(int inventorySide);
+	std::list<Inventory::Item*> GetTradeWindowItems(int inventorySide, bool stashLoaded);
+	bool ContainsItem(Inventory::TradeWindowSide side, wstring itemName, int quantity);
+	
+	Inventory::TradeWindowSide side;
+
+	virtual bool DoesItemFitInSlots(Inventory::Item* itm, int CellX, int CellY) { return false; }
+
+	virtual ~TradeInventory() {};
 };
