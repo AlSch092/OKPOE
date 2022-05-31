2caea387b0fe6a5cdbaef939fbb4ec710055059e
ALEX <94417808+AlSch092@users.noreply.github.com>
2022-05-30 22:28:38 -0400

    Update Actor.hpp

--- Components/Actor.hpp
+++ Components/Actor.hpp
@@ -15,6 +15,20 @@ namespace Actor
 		static const wchar_t* ScionPath = L"Metadata/Characters/StrDexInt/StrDexInt";
 	};
 
+	struct ActorSkill
+	{
+		UINT16 Id; //0x10;
+		int KeyDown; //0x0C
+		bool CanBeUsedWithWeapon; //0x50
+		bool CanBeUsed; //0x51 ? 0 == 1
+		int TotalUses; //0x54
+		int Cooldown; //0x5c
+		int SoulsPerUse; //0x6c
+		int TotalVaalUses; //0x70
+		bool isVaal; //
+		bool IsCastedOnSelf; //0x58?
+	};
+
 	enum ActionIDs //powers of 2
 	{
 		ACTION_NONE = 0,
@@ -28,19 +42,28 @@ namespace Actor
 		
 	};
 
-	const UINT64 ActionOffset = 0x1A8;
-	const UINT64 ActionIdOffset = 0x208;
-	const UINT64 ActionCountOffset = 0x20A;
-	const UINT64 AnimationId = 0x234;
-	const UINT64 SkillsOffset = 0x690;
-	const UINT64 VaalSkillsOffset = 0x6B0;
+	namespace Offsets
+	{
+		const UINT64 Action = 0x1A8;
+		const UINT64 ActionId = 0x208;
+		const UINT64 ActionCount = 0x20A;
+		const UINT64 AnimationId = 0x234;
+
+		const UINT64 HasMinionArrayOffset = 0x438;
+		const UINT64 DeployedObjectArrayOffset = 0x450;
+
+		const UINT64 Skills = 0x690;
+		const UINT64 VaalSkills = 0x6B0;
+	}
 
 	bool IsAttacking(Entity* e);
 	bool IsMoving(Entity* e);
 	
-	std::list<UINT16>* GetActorSkillList();
+	std::list<Actor::ActorSkill*> GetSkillList(Entity* e);
 	ActionIDs GetCurrentActionID(Entity* e);
 	int GetActionCount(Entity* e);
+
+	std::list<Entity*> GetMinions(Entity* e);
 };
 
 
