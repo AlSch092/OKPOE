5f93ae554dea8fbb0c44030982ca19ae294f2a7e
alex <94417808+AlSch092@users.noreply.github.com>
2022-06-20 02:25:01 -0400

    Update Functions.asm

--- Functions.asm
+++ Functions.asm
@@ -1,27 +1,11 @@
+;does not work as of 3.16.0 or so, very easy to fix however
+
 SendPacket PROC
-; RCX = SendClass //Only real param in func, we use 3 to make replacing ptr easier
-; RDX = Packet Buffer
-; R8 = Length
+; RCX = NetClass //Only real param in func, we use 3 to make replacing ptr easier
+; RDX =  Buff
+; R8 = Len
 	
-	sub rsp, 13F0h ;allocate space
-
-  push rax
-
-  mov rax, rcx ;SendClass
-	cmp rax,0
-	je ending
-
-  add rax, 1a8h
-	mov [rax], rdx ;swap our buffer ptr into their send class .. param 2
-	sub rax, 28h 
-  mov [rax], r8 ; length, parameter 3
-
-	call dqSendPacketFunction
-  
-ending:
-	pop rax
-	add rsp, 13F0h
-  ret
+	!removed with respect for the game developers!
 
 SendPacket ENDP
 
