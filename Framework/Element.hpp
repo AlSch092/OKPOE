df5ed4f24c5e7593266ad0f19b1c42121688b8fe
alex <94417808+AlSch092@users.noreply.github.com>
2023-01-07 00:11:09 -0500

    Update Element.hpp

--- Framework/Element.hpp
+++ Framework/Element.hpp
@@ -1,48 +1,18 @@
 #pragma once
-#include "Pointer.hpp"
+#include "ElementBase.hpp"
 
-namespace Element
+namespace Element  //unmanaged code of elements, being replaced by ElementBase class (ElementBase.hpp)
 {
-	enum Identifiers
-	{
-
-	};
-
-	static const UINT64 IdentifierOffset = 8;
-	static const UINT64 SelfPointerOffset = 0x28;
-	static const UINT64 ChildsOffset = 0x30;
-	static const UINT64 ChildEndOffset = 0x38;
-	static const UINT64 RootOffset = 0xD8;
-	static const UINT64 ParentOffset = 0xE0;
-	static const UINT64 XOffset = 0xE8;
-	static const UINT64 YOffset = 0xEC;
-	static const UINT64 ScaleOffset = 0x158;
-	static const UINT64 IsVisibleLocalOffset = 0x161;
-
-	static const UINT64 ElementBorderColorOffset = 0x160;
-	static const UINT64 ElementBackgroundColorOffset = 0x164;
-	static const UINT64 ElementOverlayColorOffset = 0x168;
-
-	static const UINT64 WidthOffset = 0x180;
-	static const UINT64 HeightOffset = 0x184;
-
-	static const UINT64 TextBoxBorderColorOffset = 0x190;
-	static const UINT64 TextBoxOverlayColorOffset = 0x194;
-
-	static const UINT64 HighlightBorderColorOffset = 0x1c0;
-	static const UINT64 isHighlightedOffset = 0x1C3;
-
-	static const UINT64 TooltipOffsetOffset = 0x3E8;
-
-	static const UINT64 EntityOffset = 0x428; //if this is present, we are an inventory item, or an item?
-	static const UINT64 CellXOffset = 0x430;
-	static const UINT64 CellYOffset = CellXOffset + 4;
-	static const UINT64 CellWidthOffset = 0x438;
-	static const UINT64 CellHeightOffset = CellWidthOffset + 4;
-	static const UINT64 IsHoveredOffset = 0x496;
-
 	void PrintHoveredElements(); 
-	void GetAllElements();
+	
 	UINT64 GetChildElement(UINT64 StartingElement);
-	UINT64 GetChildElementAtIndex(int index);
+	UINT64 GetChildElementAtIndex(UINT64 Element, int index);
+	unsigned int GetElementId(UINT64 ElementAddr);
+	Entity* GetEntityAt(UINT64 ElementBase);
+
+	std::list<ElementBase*> GetScreenElements();
+
+	wchar_t* GetLabel(UINT64 ElementAddress);
+	bool HasLabel(UINT64 ElementAddress, std::wstring label);
+	bool IsLabelOnScreen(std::wstring label);
 };
